52a363947cf1e3e53a3555a827b177b024b0b39a
Júlio César Landim <127707529+devjuliolandim@users.noreply.github.com>
2024-04-30 16:13:45 -0300

    a few cuts in unnecessary shit

--- Section 17 - The Dicee Game/index.js
+++ Section 17 - The Dicee Game/index.js
@@ -1,24 +1,18 @@
-function rollDices(){
-    let dice1 = Math.floor(Math.random() * 6) + 1;
-    let dice2 = Math.floor(Math.random() * 6) + 1;
-    let img1 = document.querySelector('.img1');
-    let img2 = document.querySelector('.img2');
-    let h1 = document.querySelector('h1');
+let dice1 = Math.floor(Math.random() * 6) + 1;
+let dice2 = Math.floor(Math.random() * 6) + 1;
+let img1 = document.querySelector('.img1');
+let img2 = document.querySelector('.img2');
+let h1 = document.querySelector('h1');
 
-    img1.setAttribute('src', './images/dice' + dice1 + '.png');
-    img2.setAttribute('src', './images/dice' + dice2 + '.png');
+img1.setAttribute('src', './images/dice' + dice1 + '.png');
+img2.setAttribute('src', './images/dice' + dice2 + '.png');
 
-    //Change H1
-    if(dice1 > dice2){
-        h1.innerHTML = "Player 1 Wins!"
-    }else if(dice2 > dice1){
-        h1.innerHTML = "Player 2 Wins!"
-    }else{
-        h1.innerHTML = "Refresh Me"
-    }
+//Change H1
+if(dice1 > dice2){
+    h1.innerHTML = "Player 1 Wins!"
+}else if(dice2 > dice1){
+    h1.innerHTML = "Player 2 Wins!"
+}else{
+    h1.innerHTML = "Draw!"
 }
 
-
-window.addEventListener('load',function(){
-    rollDices();
-})
