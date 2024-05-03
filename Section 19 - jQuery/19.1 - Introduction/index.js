a643b62ef2bd69dfdb8b2c6195ac1bfc97510ca7
Júlio César Landim <127707529+devjuliolandim@users.noreply.github.com>
2024-05-03 10:25:26 -0300

    jQuery end of the module

--- Section 19 - jQuery/19.1 - Introduction/index.js
+++ Section 19 - jQuery/19.1 - Introduction/index.js
@@ -1,2 +1,18 @@
-$("h1").css("color","red");
 $("button").css("cursor","pointer");
+console.log($("div").attr("class"));
+
+$("h1").click(function(){
+    $("h1").addClass("large")
+})
+
+$('h1').on('mouseover', function(){
+    $('h1').css('color', 'purple')
+})
+
+$('h1').on('mouseleave', function(){
+    $('h1').css('color', 'yellow')
+})
+
+$('button').on('click', function(){
+    $('h1').slideToggle();
+})
