d3f65b97ea93d11812a0c4ba4806272de7a9e46c
Júlio César Landim <127707529+devjuliolandim@users.noreply.github.com>
2024-05-04 12:43:05 -0300

    sound works

--- Section 20 - Simon Game/Simon Game Challenge Starting Files/index.js
+++ Section 20 - Simon Game/Simon Game Challenge Starting Files/index.js
@@ -1,10 +1,41 @@
+//Variables
+let levelCount = 1;
+let gameStart = false;
+let randomNumbers = [];
+
+//DOM Contants
+const BUTTONS = $(".btn");
+const HEADER = $("h1");
 
 //Main
 $(document).on('keypress', function(event){
     if(event.key === 'a'){
-        alert('the game has started');
+        
         $(document).off('keypress');
+
+
     }
 })
+
+function playSound(number){
+    switch(number){
+        case 0:
+            var audio = new Audio('sounds/green.mp3');
+            audio.play();
+        break;
+        case 1:
+            var audio = new Audio('sounds/red.mp3');
+            audio.play();
+        break;
+        case 2:
+            var audio = new Audio('sounds/yellow.mp3');
+            audio.play();
+        break;
+        case 3:
+            var audio = new Audio('sounds/blue.mp3');
+            audio.play();
+        break;          
+    }
+}
 
 
