5277bd5de2cbd1fecdcbca46507987c57f10474f
Júlio César Landim <127707529+devjuliolandim@users.noreply.github.com>
2024-05-07 16:15:34 -0300

    qr code challenge

---
+++ Backend/2.4 QR Code Project/index.js
@@ -0,0 +1,19 @@
+/* 
+1. Use the inquirer npm package to get user input.
+2. Use the qr-image npm package to turn the user entered URL into a QR code image.
+3. Create a txt file to save the user input using the native fs node module.
+*/
+import inquirer from 'inquirer'
+
+const questions = [
+    {
+        type: 'input',
+        name: 'name', 
+        message: 'what is the URL?'
+    }
+];
+
+inquirer.prompt(questions).then(answers =>{
+    const url = answers.name;
+    console.log(url);
+})
