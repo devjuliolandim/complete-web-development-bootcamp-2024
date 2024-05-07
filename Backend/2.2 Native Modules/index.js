845774c6e13855f41635f03ae0f57492017c5ace
Júlio César Landim <127707529+devjuliolandim@users.noreply.github.com>
2024-05-07 15:52:49 -0300

    npm

--- Backend/2.2 Native Modules/index.js
+++ Backend/2.2 Native Modules/index.js
@@ -1,6 +1,13 @@
 const fs =  require("fs");
+/*
+fs.writeFile('message02.txt', 'Hi, its me again!!!', (err)=>{
+    if(err) throw err;
+    console.log("The file has been saved!");
+})*/
 
-fs.readFile('message.txt','utf8',(err,data)=>{
+
+
+fs.readFile('message02.txt','utf8',(err,data)=>{
     if(err) throw err;
     console.log(data);
 })
