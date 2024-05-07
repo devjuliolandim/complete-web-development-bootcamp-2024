1d053da241e772c129791be68c0a84173e701359
Júlio César Landim <127707529+devjuliolandim@users.noreply.github.com>
2024-05-07 16:23:55 -0300

    qr code challenge done!

--- Backend/2.4 QR Code Project/index.js
+++ Backend/2.4 QR Code Project/index.js
@@ -4,6 +4,8 @@
 3. Create a txt file to save the user input using the native fs node module.
 */
 import inquirer from 'inquirer'
+import qr from 'qr-image';
+import fs from 'fs';
 
 const questions = [
     {
@@ -16,4 +18,10 @@ const questions = [
 inquirer.prompt(questions).then(answers =>{
     const url = answers.name;
     console.log(url);
+
+    const qrCode = qr.imageSync(url, {type: 'png'});
+
+    fs.writeFileSync('qrcode.png', qrCode);
+
+    console.log('QRCode saved!');
 })
