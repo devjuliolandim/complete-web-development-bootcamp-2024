782242c322d9e1f80af76275afc17210ad90a350
Júlio César Landim <127707529+devjuliolandim@users.noreply.github.com>
2024-05-08 16:09:37 -0300

    http more stuff

--- Backend/3.2 HTTP Requests/index.js
+++ Backend/3.2 HTTP Requests/index.js
@@ -3,8 +3,17 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res)=>{
-    res.send("Hello, man! Welcome to my server.");
+    res.send("<h1>Hello, World!</h1>");
 })
+
+app.get("/about", (req,res)=>{
+    res.send("<h1>My name is Júlio César</h1>");
+})
+
+app.get("/contact", (req,res)=>{
+    res.send("<h1>My e-mail is: blabla@gmail.com</h1>");
+})
+
 
 app.listen(port, ()=>{
     console.log(`The server is running in the ${port} port.`);
