9a3b1409d887e785091530a10fbfbb61042eb9cb
Júlio César Landim <127707529+devjuliolandim@users.noreply.github.com>
2024-05-10 15:55:37 -0300

    verificações middleware

---
+++ Backend/3.5 Secrets Project/index.js
@@ -0,0 +1,39 @@
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
+import express from "express";
+import {dirname} from "path";
+import { fileURLToPath, pathToFileURL } from "url";
+import bodyParser from "body-parser";
+
+const PORT = 3000;
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+function verify(req, res, next){
+    let password = req.body.password;
+
+    if(password == "ILoveProgramming"){
+        res.sendFile(__dirname + "/public/secret.html");
+    }else{
+        res.sendFile(__dirname + "/public/index.html");
+    }
+
+    next();
+}
+
+app.use(verify);
+
+app.get("/", (req, res)=>{
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", (req,res)=>{
+
+})
+
+app.listen(PORT, (req,res)  =>{
+    console.log(`The server is running in the port ${PORT}`);
+});
