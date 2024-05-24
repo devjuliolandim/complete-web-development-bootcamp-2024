5e57d50b051c8ec1efcdbe5e77d143e3a61fa1e3
Júlio César Landim <127707529+devjuliolandim@users.noreply.github.com>
2024-05-24 15:10:54 -0300

    5.6 Secrets added

--- Backend/5.5 REST APIs/index.js
+++ Backend/5.5 REST APIs/index.js
@@ -17,6 +17,10 @@ const config = {
   headers: { Authorization: `Bearer ${yourBearerToken}` },
 };
 
+function resetEJS(res){
+  res.render("index.ejs", { content: "Waiting for data..." });
+}
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
@@ -35,21 +39,80 @@ app.post("/get-secret", async (req, res) => {
 
 app.post("/post-secret", async (req, res) => {
   // TODO 2: Use axios to POST the data from req.body to the secrets api servers.
+
+  //Catch the data from the form in "index.ejs"
+  const secretForm = req.body.secret;
+  const scoreForm = req.body.score;
+
+  //Start the "try 'n' catch" block
+  try{
+    await axios.post(API_URL + '/secrets', {
+      secret: secretForm,
+      score: scoreForm
+    },config);
+  }catch(err){
+    console.error("Error to send data: " + err);
+    res.status(500).send("Error to send data. Please, try again");
+  }
+
+  resetEJS(res);
 });
 
 app.post("/put-secret", async (req, res) => {
   const searchId = req.body.id;
   // TODO 3: Use axios to PUT the data from req.body to the secrets api servers.
+
+  //Catch the data from the form in "index.ejs"
+  const secretForm = req.body.secret;
+  const scoreForm = req.body.score;
+
+  try{
+    await axios.put(API_URL + "/secrets/" + searchId, {
+      secret: secretForm,
+      score: scoreForm
+    }, config);
+  }catch(err){
+    console.error("An error has ocurred: " + err);
+    res.status(500).send("Failed to put data");
+  }
+
+  resetEJS(res);
 });
 
 app.post("/patch-secret", async (req, res) => {
   const searchId = req.body.id;
   // TODO 4: Use axios to PATCH the data from req.body to the secrets api servers.
+
+  //Catch the data from the form in "index.ejs"
+  const secretForm = req.body.secret;
+  const scoreForm = req.body.score;
+
+  try{
+    await axios.patch(API_URL + "/secrets/" + searchId, {
+      secret: secretForm,
+      score: scoreForm
+    }, config);
+  }catch(err){
+    console.error(err);
+    res.status(500).send("Failed to patch with the API");
+  }
+
+  resetEJS(res);
 });
 
 app.post("/delete-secret", async (req, res) => {
   const searchId = req.body.id;
   // TODO 5: Use axios to DELETE the item with searchId from the secrets api servers.
+
+
+  try{
+    await axios.delete(API_URL + "/secrets/" + searchId, config);
+    res.render("index.ejs", {content: `Secret with ID ${searchId} has been deleted successfully`});
+  }catch(err){
+    console.error(err);
+    res.status(500).send("Error to delete the secret");
+  }
+
 });
 
 app.listen(port, () => {
