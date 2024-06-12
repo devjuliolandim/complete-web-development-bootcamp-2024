4b0f3f43c29af8dc7c7df56d7e76cd473ddd9714
Júlio César Landim <127707529+devjuliolandim@users.noreply.github.com>
2024-06-12 15:17:29 -0300

    permalist ended

--- Backend/8.6 Permalist Project/index.js
+++ Backend/8.6 Permalist Project/index.js
@@ -21,29 +21,64 @@ db.connect();
 let items = [];
 
 //GET ALL ITEMS FROM DB
-(async()=>{
-  const response = await db.query("SELECT * FROM items");
-  items = response.rows;
-  console.log(items);
-})();
+async function getItems(){
+  try{
+    const response = await db.query("SELECT * FROM items");
 
-app.get("/", (req, res) => {
+    items = response.rows;
+  }catch(err){
+    console.error('Error to get items from DB', err);
+    throw err;
+  }
+}
+app.get("/", async (req, res) => {
+  await getItems();
   res.render("index.ejs", {
     listTitle: "Today",
     listItems: items,
   });
 });
-
-app.post("/add", (req, res) => {
+app.post("/add", async (req, res) => {
   const item = req.body.newItem;
-  items.push({ title: item });
-  res.redirect("/");
+  
+  if(!item) return res.status(400).send("Item is required")
+  
+  try{
+    await db.query("INSERT INTO items (title) VALUES ($1)", [item]);
+    res.redirect("/");
+  }catch(err){
+    console.error("Error to insert into DB", err);
+    res.status(500).send("Internal Server Error");
+  }
+
 });
+app.post("/edit", async (req, res) => {
+  const id = req.body.updatedItemId;
+  const title = req.body.updatedItemTitle;
 
-app.post("/edit", (req, res) => {});
+  if(!title) return res.status(400).send("Title is needed");
 
-app.post("/delete", (req, res) => {});
-
+  try{
+    await db.query("UPDATE items SET TITLE = $1 WHERE id = $2", [title, id]);
+    res.redirect("/");
+  }catch(err){
+    console.error("An error ocurred", err);
+    res.status(500).send("Error updating item!");
+  }
+});
+app.post("/delete", async (req, res) => {
+  const id = req.body.deleteItemId;
+  try{
+    const result = await db.query("DELETE FROM items WHERE id = $1", [id]);
+    if(result.rowCount === 0){
+      return res.status(404).send("Item not found");
+    }
+    res.redirect("/");
+  }catch(err){
+    console.error("Failed to delete item");
+    res.status(500).redirect("/");
+  }
+});
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
